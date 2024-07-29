0404ee3c72165f667deff342d6dae6fc73797504
Christian Nicolaisen <cvn_vitting@hotmail.com>
2024-07-29 09:23:18 +0200

    feat: Add columnMenuActive signal to FlexGridStore

--- lib/stores/flex_grid_store.dart
+++ lib/stores/flex_grid_store.dart
@@ -3,4 +3,5 @@ import 'package:signals/signals.dart';
 class FlexGridStore {
   static final gridEditableId = signal<String>('');
   static final hideActionsId = computed(() => gridEditableId.value);
+  static final columnMenuActive = signal<String>('-1:-1');
 }
--- lib/widgets/layouts/flex_grid_column.dart
+++ lib/widgets/layouts/flex_grid_column.dart
@@ -3,6 +3,7 @@ import 'package:flutter_web_formbuilder/enums/flex_grid_action_enum.dart';
 import 'package:flutter_web_formbuilder/extensions/element_model_extension.dart';
 import 'package:flutter_web_formbuilder/models/element_model.dart';
 import 'package:flutter_web_formbuilder/stores/application_store.dart';
+import 'package:flutter_web_formbuilder/stores/flex_grid_store.dart';
 import 'package:flutter_web_formbuilder/styles/icon_styles.dart';
 import 'package:flutter_web_formbuilder/widgets/design_view_elements.dart';
 import 'package:flutter_web_formbuilder/widgets/dialogs/delete_design_item_dialog.dart';
@@ -10,7 +11,7 @@ import 'package:flutter_web_formbuilder/widgets/layouts/flex_grid_edit.dart';
 import 'package:gap/gap.dart';
 import 'package:signals/signals_flutter.dart';
 
-class FlexGridColumn extends StatefulWidget {
+class FlexGridColumn extends StatelessWidget {
   final double columnGap;
   final bool isGridEditable;
   final ElementModel item;
@@ -25,140 +26,146 @@ class FlexGridColumn extends StatefulWidget {
     required this.columnIndex,
   });
 
-  @override
-  State<FlexGridColumn> createState() => _FlexGridColumnState();
-}
+  String get _columnSignature {
+    return '$rowIndex:$columnIndex';
+  }
 
-class _FlexGridColumnState extends State<FlexGridColumn> {
-  late String columnSignature = '${widget.rowIndex}:${widget.columnIndex}';
-  late final columnMenuActive = createSignal<String>(context, '-1:-1');
-
-  void onMenuAction(FlexGridAction value) {
+  void _onMenuAction(FlexGridAction value) {
     switch (value) {
       case FlexGridAction.addBefore:
-        final newItem = widget.item
-            .addGridColumnBefore(widget.rowIndex, widget.columnIndex);
+        final newItem = item.addGridColumnBefore(rowIndex, columnIndex);
         ApplicationStore.updateItem(newItem);
         break;
       case FlexGridAction.addAfter:
-        final newItem =
-            widget.item.addGridColumnAfter(widget.rowIndex, widget.columnIndex);
+        final newItem = item.addGridColumnAfter(rowIndex, columnIndex);
         ApplicationStore.updateItem(newItem);
         break;
       case FlexGridAction.delete:
-        final newItem =
-            widget.item.removeGridColumn(widget.rowIndex, widget.columnIndex);
+        final newItem = item.removeGridColumn(rowIndex, columnIndex);
         ApplicationStore.updateItem(newItem);
         break;
     }
   }
 
+  void _onDropBefore(ElementModel elementItem) {
+    final newItem = item.addGridChildFirstInColumn(
+      rowIndex: rowIndex,
+      columnIndex: columnIndex,
+      itemToAdd: elementItem,
+    );
+
+    ApplicationStore.updateItem(newItem);
+  }
+
+  void _onDropAfter(ElementModel elementItem, int index) {
+    final newItem = item.addGridChildAtIndexInColumn(
+      rowIndex: rowIndex,
+      columnIndex: columnIndex,
+      gridChildIndex: index,
+      itemToAdd: elementItem,
+    );
+
+    ApplicationStore.updateItem(newItem);
+  }
+
+  void _onDelete(BuildContext context, ElementModel itemToRemove) async {
+    final delete = await showDialog<bool>(
+      context: context,
+      builder: (context) {
+        return const DeleteDesignItemDialog();
+      },
+    );
+    if (delete != null && delete) {
+      final newItem = item.removeGridChildFromColumn(
+        rowIndex: rowIndex,
+        columnIndex: columnIndex,
+        itemToRemove: itemToRemove,
+      );
+
+      ApplicationStore.updateItem(newItem);
+    }
+  }
+
+  void _onReorder(int oldIndex, int newIndex) {
+    if (oldIndex < newIndex) {
+      newIndex -= 1;
+    }
+
+    final newItem = item.reorderGridChildInColumn(
+      rowIndex,
+      columnIndex,
+      oldIndex,
+      newIndex,
+    );
+
+    ApplicationStore.updateItem(newItem);
+  }
+
   @override
   Widget build(BuildContext context) {
-    final columnCount = widget.item.getGridChildColumnsCount(widget.rowIndex);
+    final columnCount = item.getGridChildColumnsCount(rowIndex);
     return Expanded(
       child: Padding(
         padding: EdgeInsets.only(
-          right: widget.columnIndex == columnCount - 1 ? 0 : widget.columnGap,
+          right: columnIndex == columnCount - 1 ? 0 : columnGap,
         ),
-        child: AnimatedContainer(
-          duration: const Duration(milliseconds: 150),
-          curve: Curves.easeIn,
-          padding:
-              EdgeInsets.all(columnMenuActive.value == columnSignature ? 8 : 4),
-          constraints: const BoxConstraints(
-            minHeight: 30,
-          ),
-          margin: EdgeInsetsGeometry.lerp(
-            const EdgeInsets.all(0),
-            const EdgeInsets.all(4),
-            columnMenuActive.value == columnSignature ? 1 : 0,
-          ),
-          decoration: BoxDecoration(
-            color: columnMenuActive.value == columnSignature
-                ? Colors.grey.shade400
-                : Colors.grey.shade200,
-            borderRadius: BorderRadius.circular(8),
-          ),
-          child: Row(
-            children: [
-              FlexGridEdit(
-                isGridEditable: widget.isGridEditable,
-                icon: IconStyles.iconTableColumn,
-                typeText: 'Column',
-                onMenuActivated: () {
-                  columnMenuActive.value = columnSignature;
-                },
-                onMenuDeactivated: () {
-                  columnMenuActive.value = '-1:-1';
-                },
-                onActionSelected: onMenuAction,
-              ),
-              if (widget.isGridEditable) const Gap(4),
-              Expanded(
-                child: DesignViewElements(
-                  parentItem: widget.item,
-                  items: widget.item
-                      .getGridChildColumn(widget.rowIndex, widget.columnIndex),
-                  highlightIdDragTargetZoneBefore:
-                      'top_${widget.columnIndex}_${widget.rowIndex}_${widget.item.id}',
-                  highlightIdDragTargetZoneAfter:
-                      '${widget.columnIndex}_${widget.rowIndex}_${widget.item.id}',
-                  onReorder: (oldIndex, newIndex) {
-                    if (oldIndex < newIndex) {
-                      newIndex -= 1;
-                    }
-
-                    final newItem = widget.item.reorderGridChildInColumn(
-                      widget.rowIndex,
-                      widget.columnIndex,
-                      oldIndex,
-                      newIndex,
-                    );
-
-                    ApplicationStore.updateItem(newItem);
+        child: Watch.builder(builder: (context) {
+          return AnimatedContainer(
+            duration: const Duration(milliseconds: 150),
+            curve: Curves.easeIn,
+            padding: EdgeInsets.all(
+                FlexGridStore.columnMenuActive.value == _columnSignature
+                    ? 8
+                    : 4),
+            constraints: const BoxConstraints(
+              minHeight: 48,
+            ),
+            margin: EdgeInsetsGeometry.lerp(
+              const EdgeInsets.all(0),
+              const EdgeInsets.all(4),
+              FlexGridStore.columnMenuActive.value == _columnSignature ? 1 : 0,
+            ),
+            decoration: BoxDecoration(
+              color: FlexGridStore.columnMenuActive.value == _columnSignature
+                  ? Colors.grey.shade400
+                  : Colors.grey.shade200,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Row(
+              children: [
+                FlexGridEdit(
+                  isGridEditable: isGridEditable,
+                  icon: IconStyles.iconTableColumn,
+                  typeText: 'Column',
+                  onMenuActivated: () {
+                    FlexGridStore.columnMenuActive.value = _columnSignature;
                   },
-                  onDropBefore: (elementItem) {
-                    final newItem = widget.item.addGridChildFirstInColumn(
-                      rowIndex: widget.rowIndex,
-                      columnIndex: widget.columnIndex,
-                      itemToAdd: elementItem,
-                    );
-
-                    ApplicationStore.updateItem(newItem);
+                  onMenuDeactivated: () {
+                    FlexGridStore.columnMenuActive.value = '-1:-1';
                   },
-                  onDropAfter: (elementItem, index) {
-                    final newItem = widget.item.addGridChildAtIndexInColumn(
-                      rowIndex: widget.rowIndex,
-                      columnIndex: widget.columnIndex,
-                      gridChildIndex: index,
-                      itemToAdd: elementItem,
-                    );
-
-                    ApplicationStore.updateItem(newItem);
-                  },
-                  onDelete: (elementItem) async {
-                    final delete = await showDialog<bool>(
-                      context: context,
-                      builder: (context) {
-                        return const DeleteDesignItemDialog();
-                      },
-                    );
-                    if (delete != null && delete) {
-                      final newItem = widget.item.removeGridChildFromColumn(
-                        rowIndex: widget.rowIndex,
-                        columnIndex: widget.columnIndex,
-                        itemToRemove: elementItem,
-                      );
-
-                      ApplicationStore.updateItem(newItem);
-                    }
-                  },
+                  onActionSelected: _onMenuAction,
                 ),
-              ),
-            ],
-          ),
-        ),
+                if (isGridEditable) const Gap(4),
+                Expanded(
+                  child: DesignViewElements(
+                    parentItem: item,
+                    items: item.getGridChildColumn(rowIndex, columnIndex),
+                    highlightIdDragTargetZoneBefore:
+                        'top_${columnIndex}_${rowIndex}_${item.id}',
+                    highlightIdDragTargetZoneAfter:
+                        '${columnIndex}_${rowIndex}_${item.id}',
+                    onReorder: _onReorder,
+                    onDropBefore: _onDropBefore,
+                    onDropAfter: _onDropAfter,
+                    onDelete: (elementItem) {
+                      _onDelete(context, elementItem);
+                    },
+                  ),
+                ),
+              ],
+            ),
+          );
+        }),
       ),
     );
   }
